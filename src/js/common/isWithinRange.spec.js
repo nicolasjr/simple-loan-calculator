802d15fea60add5ee64f8b403670541c49ce6177
Nicolas <nicolas.jreichert@gmail.com>
2017-01-11 22:49:23 +0100

    Extract repeated method call from tests

--- src/js/common/isWithinRange.spec.js
+++ src/js/common/isWithinRange.spec.js
@@ -6,27 +6,29 @@ describe('Test suite for isWithinRange method', () => {
   const max = 1000;
   const step = 10;
 
+  const checkValue = value => isWithinRange(min, max, step, value);
+
   it('returns same value when value is within range and divisible by the step', () => {
     let value = 50;
-    expect(isWithinRange(min, max, step, value)).to.equal(value);
+    expect(checkValue(value)).to.equal(value);
     value = 60;
-    expect(isWithinRange(min, max, step, value)).to.equal(value);
+    expect(checkValue(value)).to.equal(value);
     value = 150;
-    expect(isWithinRange(min, max, step, value)).to.equal(value);
+    expect(checkValue(value)).to.equal(value);
   });
 
   it('returns min when value is smaller than min', () => {
     const value = min - step;
-    expect(isWithinRange(min, max, step, value)).to.equal(min);
+    expect(checkValue(value)).to.equal(min);
   });
 
   it('returns max when value is bigger than max', () => {
     const value = max + step;
-    expect(isWithinRange(min, max, step, value)).to.equal(max);
+    expect(checkValue(value)).to.equal(max);
   });
 
   it('returns a number divisible by step when value is not', () => {
     const value = min + (step / 2);
-    expect(isWithinRange(min, max, step, value)).to.equal(min);
+    expect(checkValue(value)).to.equal(min);
   });
 });
